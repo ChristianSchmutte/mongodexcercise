e1149b8a85ce45d01e091f62b8903b869d01a5a9
Christian Schmutte <christianschmutte@gmail.com>
2021-03-19 18:45:00 +0100

    Serve static assets

--- server/index.js
+++ server/index.js
@@ -4,27 +4,39 @@ const path = require ('path');
 
 const hostname = '127.0.0.1';
 const port = 3000;
+const baseClientPath = path.join(__dirname, '..' , 'client');
 const pathNameHTML = path.join(__dirname, '..', 'client', 'index.html')
 const pathNameCSS = path.join(__dirname, '..', 'client', 'style.css')
 const pathNameJS = path.join(__dirname, '..', 'client', 'script.js')
 
+const extensions = {
+	".html" : "text/html",
+	".css" : "text/css",
+	".js" : "application/javascript",
+	".png" : "image/png",
+	".gif" : "image/gif",
+	".jpg" : "image/jpeg"
+};
 
 const server = http.createServer((req, res) => {
+    const withExt = req.url === '/' ? 'index.html' : req.url;
+    // if(req.url === '/') {
+    //   withExt = 'index.html';
+    // } else {
+    //   withExt = req.url;
+    // }
+    fs.readFile(path.join(__dirname, '..', 'client', withExt), (err,data) => {
+      if (err) {
+        console.log(err)
+        res.writeHead(404);
+        res.end();
+        return;
+      }
+        
+      res.writeHead(200);
+      res.end(data);
+    });
 
-  res.statusCode = 200;
-  // fs.readFile(pathName, (err, data) => {
-  //   if (err) throw err;
-  //   console.log(data)
-  // });
-  const htmlString = fs.readFileSync(pathNameHTML, 'utf8');
-  const jsString = fs.readFileSync(pathNameJS, 'utf8');
-  const cssString = fs.readFileSync(pathNameCSS, 'utf8');
-
-  res.setHeader('Content-Type', 'text/html');
-  res.write(htmlString);
-  res.write(cssString)
-  res.write(jsString)
-  res.end();
 });
 
 server.listen(port, hostname, () => {
