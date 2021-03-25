826192b5fd0e654b07439cebdfe80db42e1b02f1
Alexander Genkin <alexander_genkin@gmx.net>
2021-03-25 15:41:39 +0100

    add GET

--- controllers/message.controller.js
+++ controllers/message.controller.js
@@ -1,9 +1,18 @@
+const client = require('../models/index');
+const model = require('../models/message');
+
+
+
+
 function saveMessage (req, res) {
   res.send('Save message from controller');
 }
 
-function getAllMessages (req, res) {
-  res.send('Hello from GET ALLL');
+async function getAllMessages (req, res) {
+  const messages = await model.findDocuments(client);
+
+  // res.send('Hello from GET ALL');
+  res.json(messages);
 }
 
 
--- models/message.js
+++ models/message.js
@@ -11,17 +11,23 @@ const insertDocument = async function (db, callback) {
   console.log(result); // acknowledged ?
 };
 
-const findDocuments = async function(db, callback) {
-  // Get the documents collection
-  const collection = db.collection('messages');
-  // Find some documents
-  let result;
-  try {
-    result = await collection.find({}).toArray();
-  } catch (e) {
-    console.log(e);
-  }
-  console.log(result)
+const findDocuments = async function(client) {
+    await client.connect();
+    const db = client.db('messagesdb');
+    console.log('GET: Connected successfully to server');
+    // Get the documents collection
+    const collection = db.collection('messages');
+    // Find some documents
+    let result;
+    try {
+      result = await collection.find({}).toArray();
+      console.log(result)
+    } catch (e) {
+      console.log(e);
+    }
+    // console.log(result)
+    await client.close();
+    return result;
 };
 
 module.exports = {insertDocument, findDocuments};
