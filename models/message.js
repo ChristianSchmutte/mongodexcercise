933d8d6ff8c64c8e1a972153b38b5cb93531e57f
Alexander Genkin <alexander_genkin@gmx.net>
2021-03-25 13:13:03 +0100

    add message

---
+++ models/message.js
@@ -0,0 +1,27 @@
+const insertDocument = async function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('messages');
+  // Insert one documents
+  let result;
+  try {
+    result = await collection.insertOne([{ content: "testmesage", authorid: false, timestamp: Date.now() }]);
+  } catch(e) {
+    console.log(e);
+  }
+  console.log(result); // acknowledged ?
+};
+
+const findDocuments = async function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('messages');
+  // Find some documents
+  let result;
+  try {
+    result = await collection.find({}).toArray();
+  } catch (e) {
+    console.log(e);
+  }
+  console.log(result)
+};
+
+module.exports = {insertDocument, findDocuments};
