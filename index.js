381907d8264074ddc6fa197f7622eb20e17fa33b
Alexander Genkin <alexander_genkin@gmx.net>
2021-03-25 16:43:30 +0100

    add mongoose post

--- index.js
+++ index.js
@@ -4,9 +4,21 @@ const PORT = 3000;
 const router = require('./router');
 const db = require('./models/index');
 
+const mongoose = require('mongoose');
+
+
 
 app.use(express.static('./client'));
 app.use(express.json());
 app.use(router);
+
+(async function bootstrap() {
+  await mongoose.connect('mongodb://localhost/messagesdb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+})
+})();
 
 app.listen(PORT, () => console.log(`Server listening on http://localhost:${PORT}`));
--- models/message.js
+++ models/message.js
@@ -1,19 +1,46 @@
-const insertDocument = async function (client, msg) {
-  // Get the documents collection
-  await client.connect();
-  const db = client.db('messagesdb');
-  const collection = db.collection('messages');
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+
+const MessageSchema = new Schema({
+  id: ObjectId,
+  content: String,
+  auzthorid: Boolean,
+  timestamp: Number
+});
+
+const Message = mongoose.model('Message', MessageSchema);
+
+const insertDocument = async function (msg) {
+
   const {content, authorid} = msg;
-  
-  let result;
-  try {
-    result = await collection.insertOne({content, authorid, timestamp: Date.now()});
-    result = result.ops;
-  } catch(e) {
-    console.log(e);
-  }
-  console.log(result); 
-  return result
+
+  const instance = new Message();
+  instance.content = content;
+  instance.authorid = authorid;
+  instance.timestamp = Date.now();
+
+  await instance.save();
+
+
+  // const message = new Message()
+
+  // // Get the documents collection
+  // await client.connect();
+  // const db = client.db('messagesdb');
+  // const collection = db.collection('messages');
+  // const {content, authorid} = msg;
+
+  // let result;
+  // try {
+  //   result = await collection.insertOne({content, authorid, timestamp: Date.now()});
+  //   result = result.ops;
+  // } catch(e) {
+  //   console.log(e);
+  // }
+  // console.log(result);
+  // return result
 };
 
 const findDocuments = async function(client) {
